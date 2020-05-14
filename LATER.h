711cf2086b1350d1d0e129a562aba6725948282a
Orgline <Orgline@outlook.com>
2020-05-14 10:53:43 -0500

    add declarations of three QR routines

---
+++ LATER.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+
+
+void later_rgsqrf(int m, int n, float* A, int lda, float* R, int ldr);
+
+void later_rhouqr(int m, int n, float* A, int lda, float* R, int ldr);
+
+void later_bhouqr(int m, int n, float* A, int lda, float* R, int ldr);
