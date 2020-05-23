a8bcbd2fca8ee1019da13cf91bdec493a727e402
Orgline <Orgline@outlook.com>
2020-05-22 21:55:14 -0500

    Add supporting functions

---
+++ appendix/appendix.cu
@@ -0,0 +1,23 @@
+#include "LATER.h"
+
+
+
+cudaEvent_t begin, end;
+void startTimer()
+{
+    cudaEventCreate(&begin);
+    cudaEventRecord(begin);
+    cudaEventCreate(&end);
+}
+
+float stopTimer()
+{
+    
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    float milliseconds;
+    cudaEventElapsedTime(&milliseconds, begin, end);
+    cudaEventDestroy(begin);
+    cudaEventDestroy(end);
+    return milliseconds;
+}
--- include/LATER.h
+++ include/LATER.h
@@ -1,10 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cuda.h>
+#include <cublas_v2.h>
+#include <curand.h>
+#include <cuda_fp16.h>
+#include <cusolverDn.h>
+#include <cuda_runtime.h>
 
 
 
+/*
+These three functions are related with QR factorization
 
+rgsqrf: recursive Gram-Schmidt QR factorization
+
+rhouqr: recursive Householder QR factorization
+
+bhouqr: block Householder QR factorization
+*/
 void later_rgsqrf(int m, int n, float* A, int lda, float* R, int ldr);
 
 void later_rhouqr(int m, int n, float* A, int lda, float* R, int ldr);
 
 void later_bhouqr(int m, int n, float* A, int lda, float* R, int ldr);
+
+
+/*
+Below functions are the integration of often-used functions
+*/
+
+/*
+Call startTimer() at first and then call stopTimer() to get the time consumption
+
+Cannot be called nesting
+Like:
+startTimer();
+    ...
+    startTimer();
+    ...
+    stopTimer();
+    ...
+stopTimer();
+This is not supported
+*/
+void startTimer();
+
+void stopTimer();
