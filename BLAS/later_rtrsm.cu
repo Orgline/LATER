d72ac34415de61a0ac94cd87ea14e57abece92a6
Orgline <Orgline@outlook.com>
2020-08-09 06:19:57 -0500

    update rtrsm routine

--- BLAS/later_rtrsm.cu
+++ BLAS/later_rtrsm.cu
@@ -2,11 +2,78 @@
 
 #include <cuda_fp16.h>
 
-void trsm(cublasHandle_t handle, int m, int n, float* A, int lda, float* B, int ldb)
+#define BLOCKSIZE 256
 
-void later_rtrsm(int m, int n, float* A, int lda, float* B, int ldb)
+float sone = 1.0;
+float snegone = -1.0;
+float szero = 0.0;
+
+float panelTime = 0.0;
+float gemmTime = 0.0;
+
+void trsm(cublasHandle_t handle, int m, int n, float* A, int lda, float* B, int ldb, __half* hwork)
+{
+    //printf("m,n=%d,%d\n", m, n);
+    if(m <= BLOCKSIZE)
+    {
+        startTimer();
+        cublasStrsm(handle,
+            CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER,
+            CUBLAS_OP_N, CUBLAS_DIAG_NON_UNIT,
+            m, n, &sone,
+            A, lda,
+            B, ldb
+        );
+        panelTime += stopTimer();
+        //printf("%lf\n",panelTime);
+        return;
+    }
+    trsm(handle, m/2, n, A, lda, B, ldb, hwork);
+    startTimer();
+    
+    __half *Ah = hwork;
+    __half *Bh = hwork+m/2*m/2;
+
+    dim3 grid((m/2+31)/32, (m/2+31)/32);
+    dim3 block(32,32);
+    s2h<<<grid, block>>>(m/2, m/2, A+m/2, lda, Ah, m/2);
+
+    dim3 grid1((m/2+31)/32, (n+31)/32);
+    dim3 block1(32,32);
+    s2h<<<grid1, block1>>>(m/2, n, B, ldb, Bh, m/2);
+
+
+    cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, m/2, n, m/2,
+        &snegone, Ah, CUDA_R_16F, m/2, Bh, CUDA_R_16F, m/2,
+        &sone, B+m/2, CUDA_R_32F, ldb, CUDA_R_32F,
+        CUBLAS_GEMM_DEFAULT_TENSOR_OP
+    );
+
+
+    gemmTime +=stopTimer();
+    //printf("%lf\n",gemmTime);
+    //printMatrixDeviceBlock("ta.csv", m/2, m/2, A+m/2*m+m/2, lda);
+    //printMatrixDeviceBlock("tb.csv", m/2, n, B+m/2, ldb);
+    trsm(handle, m/2, n, A+m/2*m+m/2, lda, B+m/2, ldb, hwork);
+    //printf("1111111\n");
+    //printMatrixDeviceBlock("tx.csv", m/2, n, B+m/2, ldb);
+    
+}
+
+void later_rtrsm(int m, int n, float* A, int lda, float* B, int ldb, __half* hwork)
 {
     cublasHandle_t handle;
     cublasCreate(&handle);
+
+    //printMatrixDeviceBlock("A.csv", m, m, A, lda);
+    //printMatrixDeviceBlock("B.csv", m, n, B, ldb);
+    trsm(handle, m, n, A, lda, B, ldb, hwork);
+    printf("Panel takes %lfms\n", panelTime);
+    printf("Gemm takes %lfms\n", gemmTime);
+    //printf("22222\n");
+    //printMatrixDeviceBlock("X.csv", m, n, B, ldb);
+
+    cublasDestroy(handle);
+
     return;
 }
---
+++ test/test_trsm.cu
@@ -0,0 +1,137 @@
+#include "LATER.h"
+
+#include <stdlib.h>
+
+long m,n;
+bool checkFlag;
+
+int parseArguments(int argc,char *argv[])
+{
+    m = atoi(argv[1]);
+    n = atoi(argv[2]);
+    for (int i=3; i<argc; i++) {
+        if(strcmp(argv[i], "-check") == 0) {
+            checkFlag = true;
+        }
+    }
+    return 0;
+}
+
+int main(int argc,char *argv[])
+{
+    if (argc < 3) {
+        printf("Usage: test m n [options]\n");
+        printf("Options:\n\t-check: enable checking the backward error\n");
+        return 0;
+    }
+    if(parseArguments(argc,argv)!=0)
+    {
+        return 0;
+    }
+    float *A;
+    cudaMalloc(&A, sizeof(float)*m*m);
+    float *B;
+    cudaMalloc(&B, sizeof(float)*m*n);
+
+    __half *hwork;
+    cudaMalloc(&hwork, sizeof(__half)*(m/2*m/2+m/2*n));
+
+    //generateUniformMatrix(A,m,m);
+    float *hA;
+    hA = (float*)malloc(sizeof(float)*m*m);
+    for(long i=0;i<m*m;i++)
+    {
+        hA[i] = 0.1;
+    }
+    cudaMemcpy(A, hA, sizeof(float)*m*m, cudaMemcpyHostToDevice);
+    dim3 grid((m+31)/32, (m+31)/32);
+    dim3 block(32,32);
+    clearTri<<<grid, block>>>('u', m, m, A, m);
+    //printf("debug 1\n");
+    float *hB;
+    hB= (float*)malloc(sizeof(float)*m*n);
+
+    for(long i=0;i<m*n;i++)
+        //hB[i] = rand()/(RAND_MAX+1.0)/m;
+        hB[i] = 1.0;
+
+    cudaMemcpy(B, hB, sizeof(float)*m*n, cudaMemcpyHostToDevice);
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    //printf("debug 1\n");
+    float sone = 1.0;
+    float snegone = -1.0;
+    float szero = 0.0;
+
+    float *work;
+    cudaMalloc(&work, sizeof(float)*m*n);
+    //printf("debug 1\n");
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, m,
+        &sone, A, m, B, m,
+        &szero, work, m
+    );
+    cudaMemcpy(B, work, sizeof(float)*m*n, cudaMemcpyDeviceToDevice);
+
+    //generateNormalMatrix(B,m,n);
+    //printMatrixDeviceBlock("A.csv", m, m, A, m);
+    //printMatrixDeviceBlock("B.csv", m, n, B, m);
+    later_rtrsm(m, n, A, m, B, m, hwork);
+    //printMatrixDeviceBlock("X.csv", m, n, B, m);
+    //printf("debug 1\n");
+    if(checkFlag)
+    {
+        //printf("Check backward error\n");
+        float *dB;
+        cudaMalloc(&dB, sizeof(float)*m*n);
+        
+        //generateNormalMatrix(dB,m,n);
+        cudaMemcpy(dB, hB, sizeof(float)*m*n, cudaMemcpyHostToDevice);
+        cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, m,
+            &sone, A, m, dB, m,
+            &szero, work, m
+        );
+        cudaMemcpy(dB, work, sizeof(float)*m*n, cudaMemcpyDeviceToDevice);
+
+        float normB = snorm(m, n, dB);
+        //printf("%lf\n", normB);
+
+        
+        //printMatrixDeviceBlock("dA.csv", m, m, A, m);
+        //printMatrixDeviceBlock("dB.csv", m, n, dB, m);
+        //printMatrixDeviceBlock("dX.csv", m, n, B, m);
+        
+        cublasSgemm(handle, 
+            CUBLAS_OP_N, CUBLAS_OP_N, 
+            m, n, m,
+            &snegone, A, m,
+            B, m,
+            &sone, dB, m
+        );
+        //printMatrixDeviceBlock("bb.csv", m, n, dB, m);
+        //printf("norm db = %.6e\n", snorm(m,n,dB));
+        //cudaFree(dB);
+        printf("Backward error ||A*X-B||/||B|| is %.6e\n", snorm(m,n,dB)/normB);
+        cudaFree(dB);
+    }
+
+    {
+        startTimer();
+        cublasStrsm(handle,
+            CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER,
+            CUBLAS_OP_N, CUBLAS_DIAG_NON_UNIT,
+            m, n, &sone,
+            A, m,
+            B, m
+        );
+        printf("cuSOLVER strsm takes %lf\n", stopTimer());
+    }
+
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(work);
+    cudaFree(hwork);
+    cublasDestroy(handle);
+    
+    return 0;
+}
